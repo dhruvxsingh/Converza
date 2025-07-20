e6ab599adcd4fab76cfe31184eccfdcc8869dc90
Dhruv Singh <dhruv.competent@gmail.com>
2025-07-20 23:11:25 +0530

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import { Platform } from 'react-native';
+
+
+if (Platform.OS !== 'web') {
+  try {
+    const { registerGlobals } = require('react-native-webrtc');
+    registerGlobals();
+    console.log('WebRTC initialized successfully');
+  } catch (error) {
+    console.log('WebRTC initialization error:', error);
+  }
+}
+
 
 export default function App() {
   return <AppNavigator />;
